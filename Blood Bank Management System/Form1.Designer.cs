ef2b3b937ac8ef1f2e3292da407f6479128cc071
TecHMooN <TecHMooN@ZAHIN>
2024-12-25 14:00:40 +0600

    Add project files.

---
+++ Blood Bank Management System/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Blood_Bank_Management_System
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panel1 = new Panel();
+            btnHideShow = new LinkLabel();
+            btnLogin = new Button();
+            checkBox = new CheckBox();
+            txtPassword = new TextBox();
+            label2 = new Label();
+            txtUsername = new TextBox();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.AutoSize = true;
+            panel1.BackColor = SystemColors.ControlLight;
+            panel1.Controls.Add(btnHideShow);
+            panel1.Controls.Add(btnLogin);
+            panel1.Controls.Add(checkBox);
+            panel1.Controls.Add(txtPassword);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(txtUsername);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Location = new Point(184, 76);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(600, 800);
+            panel1.TabIndex = 0;
+            // 
+            // btnHideShow
+            // 
+            btnHideShow.AutoSize = true;
+            btnHideShow.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnHideShow.LinkColor = Color.Black;
+            btnHideShow.Location = new Point(390, 377);
+            btnHideShow.Name = "btnHideShow";
+            btnHideShow.Size = new Size(56, 25);
+            btnHideShow.TabIndex = 7;
+            btnHideShow.TabStop = true;
+            btnHideShow.Text = "Show";
+            btnHideShow.TextAlign = ContentAlignment.MiddleRight;
+            btnHideShow.LinkClicked += linkLabel1_LinkClicked;
+            // 
+            // btnLogin
+            // 
+            btnLogin.BackColor = SystemColors.HighlightText;
+            btnLogin.Cursor = Cursors.Hand;
+            btnLogin.Location = new Point(251, 531);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(112, 34);
+            btnLogin.TabIndex = 6;
+            btnLogin.Text = "Login";
+            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += button1_Click;
+            btnLogin.MouseHover += button1_Click;
+            // 
+            // checkBox
+            // 
+            checkBox.AutoSize = true;
+            checkBox.Font = new Font("Times New Roman", 10F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            checkBox.Location = new Point(166, 460);
+            checkBox.Name = "checkBox";
+            checkBox.Size = new Size(280, 27);
+            checkBox.TabIndex = 5;
+            checkBox.Text = "Accept Terms and Conditions";
+            checkBox.UseVisualStyleBackColor = true;
+            checkBox.CheckedChanged += checkBox1_CheckedChanged;
+            checkBox.CheckStateChanged += checkBox_CheckStateChanged;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(166, 405);
+            txtPassword.Name = "txtPassword";
+            txtPassword.PasswordChar = '*';
+            txtPassword.Size = new Size(280, 31);
+            txtPassword.TabIndex = 4;
+            txtPassword.TextAlign = HorizontalAlignment.Center;
+            txtPassword.TextChanged += txtPassword_TextChanged;
+            // 
+            // label2
+            // 
+            label2.Font = new Font("Times New Roman", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Image = (Image)resources.GetObject("label2.Image");
+            label2.ImageAlign = ContentAlignment.MiddleLeft;
+            label2.Location = new Point(166, 377);
+            label2.Name = "label2";
+            label2.Size = new Size(129, 25);
+            label2.TabIndex = 3;
+            label2.Text = "Password";
+            label2.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new Point(166, 294);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(280, 31);
+            txtUsername.TabIndex = 2;
+            txtUsername.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            label1.Font = new Font("Times New Roman", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Image = (Image)resources.GetObject("label1.Image");
+            label1.ImageAlign = ContentAlignment.MiddleLeft;
+            label1.Location = new Point(166, 253);
+            label1.Name = "label1";
+            label1.Size = new Size(132, 38);
+            label1.TabIndex = 1;
+            label1.Text = "Username";
+            label1.TextAlign = ContentAlignment.MiddleRight;
+            label1.Click += label1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(156, 3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(300, 143);
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(1258, 667);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.SizableToolWindow;
+            Name = "Form1";
+            Text = "Login";
+            WindowState = FormWindowState.Maximized;
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private PictureBox pictureBox1;
+        private CheckBox checkBox;
+        private TextBox txtPassword;
+        private Label label2;
+        private TextBox txtUsername;
+        private Label label1;
+        private Button btnLogin;
+        private LinkLabel btnHideShow;
+        private Button button1;
+    }
+}
---
+++ Blood Bank Management System/Form1.cs
@@ -0,0 +1,78 @@
+using System.Security.Cryptography;
+
+namespace Blood_Bank_Management_System
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "Emtiaz" && txtPassword.Text == "pass")
+            {
+                Dashboard db = new Dashboard();
+                db.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Enter valid username or password", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if (btnHideShow.Text == "Show")
+            {
+                btnHideShow.Text = "Hide";
+                txtPassword.PasswordChar = '\0';
+            }
+            else
+            {
+                btnHideShow.Text = "Show";
+                txtPassword.PasswordChar = '*';
+            }
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox_CheckStateChanged(object sender, EventArgs e)
+        {
+            if (checkBox.Checked == true)
+            {
+                btnLogin.Enabled = true;
+            }
+            else
+            {
+                btnLogin.Enabled = false;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnLogin.Enabled = false;
+        }
+    }
+}
