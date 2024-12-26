c117592440019d08296a8b8d8e663700d812f032
Emtiaz Ahmed <155218645+Emtiaz01@users.noreply.github.com>
2024-12-27 00:23:41 +0600

    Add files via upload

---
+++ UpdateDonorDetails.Designer.cs
@@ -0,0 +1,417 @@
+﻿namespace Blood_Bank_Management_System
+{
+    partial class btnSave
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(btnSave));
+            label1 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            Text1 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            label14 = new Label();
+            txtName = new TextBox();
+            txtFather = new TextBox();
+            txtMother = new TextBox();
+            txtEmail = new TextBox();
+            txtAddress = new RichTextBox();
+            txtCity = new TextBox();
+            txtMobile = new TextBox();
+            txtGender = new ComboBox();
+            txtDOB = new DateTimePicker();
+            txtDonorID = new TextBox();
+            btnReset = new Button();
+            btnClose = new Button();
+            btnSearch = new Button();
+            btnUpdate = new Button();
+            txtBloodGroup = new ComboBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.LemonChiffon;
+            label1.Font = new Font("Times New Roman", 20F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(384, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(387, 45);
+            label1.TabIndex = 0;
+            label1.Text = "Update Donor Details";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.LemonChiffon;
+            label3.Font = new Font("Times New Roman", 12F, FontStyle.Bold);
+            label3.Location = new Point(114, 178);
+            label3.Name = "label3";
+            label3.Size = new Size(71, 26);
+            label3.TabIndex = 1;
+            label3.Text = "Name";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.LemonChiffon;
+            label4.Font = new Font("Times New Roman", 12F, FontStyle.Bold);
+            label4.Location = new Point(114, 253);
+            label4.Name = "label4";
+            label4.Size = new Size(163, 26);
+            label4.TabIndex = 1;
+            label4.Text = "Father's Name";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.LemonChiffon;
+            label5.Font = new Font("Times New Roman", 12F, FontStyle.Bold);
+            label5.Location = new Point(114, 323);
+            label5.Name = "label5";
+            label5.Size = new Size(171, 26);
+            label5.TabIndex = 1;
+            label5.Text = "Mother's Name";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.LemonChiffon;
+            label6.Font = new Font("Times New Roman", 12F, FontStyle.Bold);
+            label6.Location = new Point(114, 400);
+            label6.Name = "label6";
+            label6.Size = new Size(146, 26);
+            label6.TabIndex = 1;
+            label6.Text = "Date of Birth";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = Color.LemonChiffon;
+            label7.Font = new Font("Times New Roman", 12F, FontStyle.Bold);
+            label7.Location = new Point(114, 467);
+            label7.Name = "label7";
+            label7.Size = new Size(120, 26);
+            label7.TabIndex = 1;
+            label7.Text = "Mobile No";
+            label7.Click += label7_Click;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.BackColor = Color.LemonChiffon;
+            label8.Font = new Font("Times New Roman", 12F, FontStyle.Bold);
+            label8.Location = new Point(114, 537);
+            label8.Name = "label8";
+            label8.Size = new Size(89, 26);
+            label8.TabIndex = 1;
+            label8.Text = "Gender";
+            // 
+            // Text1
+            // 
+            Text1.AutoSize = true;
+            Text1.BackColor = Color.LemonChiffon;
+            Text1.Font = new Font("Times New Roman", 12F, FontStyle.Bold);
+            Text1.Location = new Point(362, 82);
+            Text1.Name = "Text1";
+            Text1.Size = new Size(109, 26);
+            Text1.TabIndex = 1;
+            Text1.Text = "Donor ID";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.BackColor = Color.LemonChiffon;
+            label11.Font = new Font("Times New Roman", 12F, FontStyle.Bold);
+            label11.Location = new Point(687, 255);
+            label11.Name = "label11";
+            label11.Size = new Size(145, 26);
+            label11.TabIndex = 1;
+            label11.Text = "Blood Group";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.BackColor = Color.LemonChiffon;
+            label12.Font = new Font("Times New Roman", 12F, FontStyle.Bold);
+            label12.Location = new Point(687, 400);
+            label12.Name = "label12";
+            label12.Size = new Size(95, 26);
+            label12.TabIndex = 1;
+            label12.Text = "Address";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.BackColor = Color.LemonChiffon;
+            label13.Font = new Font("Times New Roman", 12F, FontStyle.Bold);
+            label13.Location = new Point(687, 339);
+            label13.Name = "label13";
+            label13.Size = new Size(56, 26);
+            label13.TabIndex = 1;
+            label13.Text = "City";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.BackColor = Color.LemonChiffon;
+            label14.Font = new Font("Times New Roman", 12F, FontStyle.Bold);
+            label14.Location = new Point(687, 178);
+            label14.Name = "label14";
+            label14.Size = new Size(73, 26);
+            label14.TabIndex = 1;
+            label14.Text = "Email";
+            // 
+            // txtName
+            // 
+            txtName.BackColor = Color.LemonChiffon;
+            txtName.Location = new Point(313, 178);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(240, 31);
+            txtName.TabIndex = 3;
+            // 
+            // txtFather
+            // 
+            txtFather.BackColor = Color.LemonChiffon;
+            txtFather.Location = new Point(315, 250);
+            txtFather.Name = "txtFather";
+            txtFather.Size = new Size(240, 31);
+            txtFather.TabIndex = 3;
+            // 
+            // txtMother
+            // 
+            txtMother.BackColor = Color.LemonChiffon;
+            txtMother.Location = new Point(315, 323);
+            txtMother.Name = "txtMother";
+            txtMother.Size = new Size(240, 31);
+            txtMother.TabIndex = 3;
+            // 
+            // txtEmail
+            // 
+            txtEmail.BackColor = Color.LemonChiffon;
+            txtEmail.Location = new Point(860, 178);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(240, 31);
+            txtEmail.TabIndex = 3;
+            // 
+            // txtAddress
+            // 
+            txtAddress.BackColor = Color.LemonChiffon;
+            txtAddress.Location = new Point(860, 400);
+            txtAddress.Name = "txtAddress";
+            txtAddress.Size = new Size(238, 144);
+            txtAddress.TabIndex = 6;
+            txtAddress.Text = "";
+            // 
+            // txtCity
+            // 
+            txtCity.BackColor = Color.LemonChiffon;
+            txtCity.Location = new Point(860, 337);
+            txtCity.Name = "txtCity";
+            txtCity.Size = new Size(240, 31);
+            txtCity.TabIndex = 3;
+            // 
+            // txtMobile
+            // 
+            txtMobile.BackColor = Color.LemonChiffon;
+            txtMobile.Location = new Point(313, 467);
+            txtMobile.Name = "txtMobile";
+            txtMobile.Size = new Size(240, 31);
+            txtMobile.TabIndex = 3;
+            // 
+            // txtGender
+            // 
+            txtGender.BackColor = Color.LemonChiffon;
+            txtGender.DropDownStyle = ComboBoxStyle.DropDownList;
+            txtGender.FormattingEnabled = true;
+            txtGender.Items.AddRange(new object[] { "Male", "Female", "Others" });
+            txtGender.Location = new Point(317, 535);
+            txtGender.Name = "txtGender";
+            txtGender.Size = new Size(238, 33);
+            txtGender.TabIndex = 7;
+            // 
+            // txtDOB
+            // 
+            txtDOB.CalendarMonthBackground = Color.LemonChiffon;
+            txtDOB.CalendarTitleBackColor = Color.LemonChiffon;
+            txtDOB.CalendarTitleForeColor = Color.AliceBlue;
+            txtDOB.Format = DateTimePickerFormat.Short;
+            txtDOB.Location = new Point(313, 400);
+            txtDOB.Name = "txtDOB";
+            txtDOB.Size = new Size(240, 31);
+            txtDOB.TabIndex = 8;
+            // 
+            // txtDonorID
+            // 
+            txtDonorID.BackColor = Color.LemonChiffon;
+            txtDonorID.Location = new Point(477, 80);
+            txtDonorID.Name = "txtDonorID";
+            txtDonorID.Size = new Size(240, 31);
+            txtDonorID.TabIndex = 3;
+            txtDonorID.TextChanged += txtDonorID_TextChanged;
+            // 
+            // btnReset
+            // 
+            btnReset.BackColor = Color.LemonChiffon;
+            btnReset.Image = (Image)resources.GetObject("btnReset.Image");
+            btnReset.ImageAlign = ContentAlignment.MiddleLeft;
+            btnReset.Location = new Point(548, 615);
+            btnReset.Name = "btnReset";
+            btnReset.Size = new Size(112, 34);
+            btnReset.TabIndex = 10;
+            btnReset.Text = "Reset";
+            btnReset.UseVisualStyleBackColor = false;
+            btnReset.Click += btnReset_Click;
+            // 
+            // btnClose
+            // 
+            btnClose.BackColor = Color.LemonChiffon;
+            btnClose.Image = (Image)resources.GetObject("btnClose.Image");
+            btnClose.ImageAlign = ContentAlignment.MiddleLeft;
+            btnClose.Location = new Point(860, 615);
+            btnClose.Name = "btnClose";
+            btnClose.RightToLeft = RightToLeft.No;
+            btnClose.Size = new Size(112, 34);
+            btnClose.TabIndex = 11;
+            btnClose.Text = "Close";
+            btnClose.UseVisualStyleBackColor = false;
+            btnClose.Click += button3_Click;
+            // 
+            // btnSearch
+            // 
+            btnSearch.BackColor = Color.LemonChiffon;
+            btnSearch.Image = (Image)resources.GetObject("btnSearch.Image");
+            btnSearch.ImageAlign = ContentAlignment.MiddleLeft;
+            btnSearch.Location = new Point(723, 78);
+            btnSearch.Name = "btnSearch";
+            btnSearch.Size = new Size(112, 34);
+            btnSearch.TabIndex = 12;
+            btnSearch.Text = "Search";
+            btnSearch.UseVisualStyleBackColor = false;
+            btnSearch.Click += btnSearch_Click;
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.BackColor = Color.LemonChiffon;
+            btnUpdate.Image = (Image)resources.GetObject("btnUpdate.Image");
+            btnUpdate.ImageAlign = ContentAlignment.MiddleLeft;
+            btnUpdate.Location = new Point(239, 615);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(112, 34);
+            btnUpdate.TabIndex = 13;
+            btnUpdate.Text = "Update";
+            btnUpdate.UseVisualStyleBackColor = false;
+            btnUpdate.Click += btnSave_Click;
+            // 
+            // txtBloodGroup
+            // 
+            txtBloodGroup.BackColor = Color.LemonChiffon;
+            txtBloodGroup.DropDownStyle = ComboBoxStyle.DropDownList;
+            txtBloodGroup.FormattingEnabled = true;
+            txtBloodGroup.Items.AddRange(new object[] { "A", "A+", "A-", "B", "B+", "B-", "O", "O+", "O-", "AB", "AB+", "AB-" });
+            txtBloodGroup.Location = new Point(860, 246);
+            txtBloodGroup.Name = "txtBloodGroup";
+            txtBloodGroup.Size = new Size(238, 33);
+            txtBloodGroup.TabIndex = 7;
+            // 
+            // btnSave
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(1174, 693);
+            Controls.Add(btnUpdate);
+            Controls.Add(btnSearch);
+            Controls.Add(btnClose);
+            Controls.Add(btnReset);
+            Controls.Add(txtDOB);
+            Controls.Add(txtBloodGroup);
+            Controls.Add(txtGender);
+            Controls.Add(txtAddress);
+            Controls.Add(txtMobile);
+            Controls.Add(txtCity);
+            Controls.Add(txtEmail);
+            Controls.Add(txtMother);
+            Controls.Add(txtFather);
+            Controls.Add(txtDonorID);
+            Controls.Add(txtName);
+            Controls.Add(label14);
+            Controls.Add(label13);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(Text1);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.SizableToolWindow;
+            Name = "btnSave";
+            Text = "UpdateDonorDetails";
+            Load += UpdateDonorDetails_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label Text1;
+        private Label label11;
+        private Label label12;
+        private Label label13;
+        private Label label14;
+        private TextBox txtName;
+        private TextBox txtFather;
+        private TextBox txtMother;
+        private TextBox txtEmail;
+        private RichTextBox txtAddress;
+        private TextBox txtCity;
+        private TextBox txtMobile;
+        private ComboBox txtGender;
+        private DateTimePicker txtDOB;
+        private TextBox txtDonorID;
+        private Button btnReset;
+        private Button btnClose;
+        private Button btnSearch;
+        private Button btnUpdate;
+        private ComboBox txtBloodGroup;
+    }
+}
---
+++ UpdateDonorDetails.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Blood_Bank_Management_System
+{
+    public partial class btnSave : Form
+    {
+        function fn = new function();
+        public btnSave()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Name_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtDonorID.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void UpdateDonorDetails_Load(object sender, EventArgs e)
+        {
+            txtDonorID.Clear();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtDonorID.Text.ToString());
+            String query = "select * from BdDonor where Did = " + id + "";
+            DataSet ds = fn.getData(query);
+
+            if (ds.Tables[0].Rows.Count != 0)
+            {
+                txtName.Text = ds.Tables[0].Rows[0][1].ToString();
+                txtFather.Text = ds.Tables[0].Rows[0][2].ToString();
+                txtMother.Text = ds.Tables[0].Rows[0][3].ToString();
+                txtDOB.Text = ds.Tables[0].Rows[0][4].ToString();
+                txtMobile.Text = ds.Tables[0].Rows[0][5].ToString();
+                txtGender.Text = ds.Tables[0].Rows[0][6].ToString();
+                txtEmail.Text = ds.Tables[0].Rows[0][7].ToString();
+                txtBloodGroup.Text = ds.Tables[0].Rows[0][8].ToString();
+                txtCity.Text = ds.Tables[0].Rows[0][9].ToString();
+                txtAddress.Text = ds.Tables[0].Rows[0][10].ToString();
+            }
+            else
+            {
+                MessageBox.Show("Invalid ID", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            String query = "update BdDonor set Dname = '"+txtName.Text+ "',Fname = '" +txtFather.Text+ "',Mname = '" +txtMother.Text+ "', Dob = '" +txtDOB.Text+ "', Mobile = '" +txtMobile.Text+ "', Gender = '" +txtGender.Text+ "',Email = '" +txtEmail.Text+ "', BloodGroup = '" +txtBloodGroup.Text+ "',City = '" +txtCity.Text+ "',DAddress = '" +txtAddress.Text+ "'where Did = " +txtDonorID.Text+ "";
+            fn.setData(query);
+            UpdateDonorDetails_Load(this, null);
+        }
+
+        private void txtDonorID_TextChanged(object sender, EventArgs e)
+        {
+            if (txtDonorID.Text == "")
+            {
+                txtName.Clear();
+                txtFather.Clear();
+                txtMother.Clear();
+                txtDOB.ResetText();
+                txtMobile.Clear();
+                txtGender.ResetText();
+                txtEmail.Clear();
+                txtBloodGroup.ResetText();
+                txtCity.Clear();
+                txtAddress.Clear();
+            }
+        }
+        
+    }
+}
+
