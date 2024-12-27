eeac2f937c68788c0c316892a710c4789a567520
Emtiaz Ahmed <155218645+Emtiaz01@users.noreply.github.com>
2024-12-27 22:25:24 +0600

    Search Donor by Address

---
+++ Blood Bank Management System/SearchBloodOnorAddress.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Blood_Bank_Management_System
+{
+    partial class SearchBloodOnorAddress
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SearchBloodOnorAddress));
+            label1 = new Label();
+            dataGridView1 = new DataGridView();
+            btnPrint = new Button();
+            btnClose = new Button();
+            txtAddress = new TextBox();
+            label2 = new Label();
+            printDocument1 = new System.Drawing.Printing.PrintDocument();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.LemonChiffon;
+            label1.Font = new Font("Times New Roman", 20F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ActiveCaptionText;
+            label1.Location = new Point(411, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(535, 45);
+            label1.TabIndex = 0;
+            label1.Text = "Search Blood Donor (Address)";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 133);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.Size = new Size(1357, 459);
+            dataGridView1.TabIndex = 1;
+            // 
+            // btnPrint
+            // 
+            btnPrint.BackColor = Color.LemonChiffon;
+            btnPrint.Font = new Font("Times New Roman", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnPrint.ForeColor = SystemColors.ActiveCaptionText;
+            btnPrint.Image = (Image)resources.GetObject("btnPrint.Image");
+            btnPrint.ImageAlign = ContentAlignment.MiddleLeft;
+            btnPrint.Location = new Point(108, 615);
+            btnPrint.Name = "btnPrint";
+            btnPrint.Size = new Size(112, 34);
+            btnPrint.TabIndex = 2;
+            btnPrint.Text = "Print";
+            btnPrint.UseVisualStyleBackColor = false;
+            btnPrint.Click += button1_Click;
+            // 
+            // btnClose
+            // 
+            btnClose.BackColor = Color.LemonChiffon;
+            btnClose.Font = new Font("Times New Roman", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnClose.ForeColor = SystemColors.ActiveCaptionText;
+            btnClose.Image = (Image)resources.GetObject("btnClose.Image");
+            btnClose.ImageAlign = ContentAlignment.MiddleLeft;
+            btnClose.Location = new Point(1171, 615);
+            btnClose.Name = "btnClose";
+            btnClose.Size = new Size(112, 34);
+            btnClose.TabIndex = 3;
+            btnClose.Text = "Close";
+            btnClose.UseVisualStyleBackColor = false;
+            btnClose.Click += btnClose_Click;
+            // 
+            // txtAddress
+            // 
+            txtAddress.BackColor = Color.LemonChiffon;
+            txtAddress.Location = new Point(578, 76);
+            txtAddress.Name = "txtAddress";
+            txtAddress.Size = new Size(316, 31);
+            txtAddress.TabIndex = 4;
+            txtAddress.TextChanged += textBox1_TextChanged;
+            // 
+            // label2
+            // 
+            label2.BackColor = Color.LemonChiffon;
+            label2.Font = new Font("Times New Roman", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = SystemColors.ActiveCaptionText;
+            label2.Image = (Image)resources.GetObject("label2.Image");
+            label2.ImageAlign = ContentAlignment.MiddleLeft;
+            label2.Location = new Point(465, 76);
+            label2.Name = "label2";
+            label2.Size = new Size(107, 31);
+            label2.TabIndex = 5;
+            label2.Text = "Address";
+            label2.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // printDocument1
+            // 
+            printDocument1.PrintPage += printDocument1_PrintPage;
+            // 
+            // SearchBloodOnorAddress
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(1381, 695);
+            Controls.Add(label2);
+            Controls.Add(txtAddress);
+            Controls.Add(btnClose);
+            Controls.Add(btnPrint);
+            Controls.Add(dataGridView1);
+            Controls.Add(label1);
+            ForeColor = SystemColors.ActiveCaptionText;
+            FormBorderStyle = FormBorderStyle.SizableToolWindow;
+            Name = "SearchBloodOnorAddress";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "SearchBloodOnorAddress";
+            Load += SearchBloodOnorAddress_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private DataGridView dataGridView1;
+        private Button btnPrint;
+        private Button btnClose;
+        private TextBox txtAddress;
+        private Label label2;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+    }
+}
---
+++ Blood Bank Management System/SearchBloodOnorAddress.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Blood_Bank_Management_System
+{
+    public partial class SearchBloodOnorAddress : Form
+    {
+        function fn = new function();
+        public SearchBloodOnorAddress()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (txtAddress.Text != "")
+            {
+                String query = "select * from BdDonor where City Like '" + txtAddress.Text + "%' or Daddress Like '" + txtAddress.Text + "%'";
+                DataSet ds = fn.getData(query);
+                dataGridView1.DataSource = ds.Tables[0];
+            }
+            else
+            {
+                String query = "select * from BdDonor";
+                DataSet ds = fn.getData(query);
+                dataGridView1.DataSource = ds.Tables[0];
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            printDocument1.Print();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void SearchBloodOnorAddress_Load(object sender, EventArgs e)
+        {
+            String query = "Select * from BdDonor";
+            DataSet ds = fn.getData(query);
+
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            Bitmap bm = new Bitmap(this.dataGridView1.Width, this.dataGridView1.Height);
+            dataGridView1.DrawToBitmap(bm, new Rectangle(0, 0, this.dataGridView1.Width, this.dataGridView1.Height));
+            e.Graphics.DrawImage(bm, 0, 0); 
+        }
+    }
+}
