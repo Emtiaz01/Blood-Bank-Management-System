c1bbfedadd8c4060ab99d9cf78ab6053efcc8f94
Emtiaz Ahmed <155218645+Emtiaz01@users.noreply.github.com>
2024-12-27 11:17:37 +0600

    Add files via upload

---
+++ Blood Bank Management System/AllDonorDetails.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Blood_Bank_Management_System
+{
+    partial class AllDonorDetails
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AllDonorDetails));
+            label1 = new Label();
+            dataGridView1 = new DataGridView();
+            btnPrint = new Button();
+            btnclose = new Button();
+            printDocument1 = new System.Drawing.Printing.PrintDocument();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.LemonChiffon;
+            label1.Font = new Font("Times New Roman", 20F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Black;
+            label1.Location = new Point(554, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(314, 45);
+            label1.TabIndex = 0;
+            label1.Text = "All Donor Details";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 66);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.Size = new Size(1378, 521);
+            dataGridView1.TabIndex = 1;
+            // 
+            // btnPrint
+            // 
+            btnPrint.Font = new Font("Times New Roman", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnPrint.Image = (Image)resources.GetObject("btnPrint.Image");
+            btnPrint.ImageAlign = ContentAlignment.MiddleLeft;
+            btnPrint.Location = new Point(40, 627);
+            btnPrint.Name = "btnPrint";
+            btnPrint.Size = new Size(112, 34);
+            btnPrint.TabIndex = 2;
+            btnPrint.Text = "Print";
+            btnPrint.UseVisualStyleBackColor = true;
+            btnPrint.Click += btnPrint_Click;
+            // 
+            // btnclose
+            // 
+            btnclose.Font = new Font("Times New Roman", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnclose.Image = (Image)resources.GetObject("btnclose.Image");
+            btnclose.ImageAlign = ContentAlignment.MiddleLeft;
+            btnclose.Location = new Point(1242, 627);
+            btnclose.Name = "btnclose";
+            btnclose.Size = new Size(112, 34);
+            btnclose.TabIndex = 2;
+            btnclose.Text = "Close";
+            btnclose.UseVisualStyleBackColor = true;
+            btnclose.Click += btnclose_Click;
+            // 
+            // printDocument1
+            // 
+            printDocument1.PrintPage += printDocument1_PrintPage;
+            // 
+            // AllDonorDetails
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(1425, 732);
+            Controls.Add(btnclose);
+            Controls.Add(btnPrint);
+            Controls.Add(dataGridView1);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.SizableToolWindow;
+            Name = "AllDonorDetails";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "AllDonorDetails";
+            Load += AllDonorDetails_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private DataGridView dataGridView1;
+        private Button btnPrint;
+        private Button btnclose;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+    }
+}
