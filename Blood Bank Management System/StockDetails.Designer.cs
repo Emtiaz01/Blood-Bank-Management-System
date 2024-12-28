7d6f5df3fa6424608ba1129c92cf7aa505007f09
Emtiaz Ahmed <155218645+Emtiaz01@users.noreply.github.com>
2024-12-28 13:56:42 +0600

    Stock Complete with Stock Details

---
+++ Blood Bank Management System/StockDetails.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Blood_Bank_Management_System
+{
+    partial class StockDetails
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StockDetails));
+            label1 = new Label();
+            btnPrint = new Button();
+            btnClose = new Button();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.LemonChiffon;
+            label1.Font = new Font("Times New Roman", 20F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(536, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(243, 45);
+            label1.TabIndex = 0;
+            label1.Text = "Stock Details";
+            // 
+            // btnPrint
+            // 
+            btnPrint.BackColor = Color.LemonChiffon;
+            btnPrint.Font = new Font("Times New Roman", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnPrint.Image = (Image)resources.GetObject("btnPrint.Image");
+            btnPrint.ImageAlign = ContentAlignment.MiddleLeft;
+            btnPrint.Location = new Point(149, 590);
+            btnPrint.Name = "btnPrint";
+            btnPrint.Size = new Size(112, 45);
+            btnPrint.TabIndex = 1;
+            btnPrint.Text = "Print";
+            btnPrint.UseVisualStyleBackColor = false;
+            btnPrint.Click += button1_Click;
+            // 
+            // btnClose
+            // 
+            btnClose.BackColor = Color.LemonChiffon;
+            btnClose.Font = new Font("Times New Roman", 11F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnClose.Image = (Image)resources.GetObject("btnClose.Image");
+            btnClose.ImageAlign = ContentAlignment.MiddleLeft;
+            btnClose.Location = new Point(1075, 590);
+            btnClose.Name = "btnClose";
+            btnClose.Size = new Size(112, 45);
+            btnClose.TabIndex = 2;
+            btnClose.Text = "Close";
+            btnClose.UseVisualStyleBackColor = false;
+            btnClose.Click += button2_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 90);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.Size = new Size(1317, 476);
+            dataGridView1.TabIndex = 3;
+            // 
+            // StockDetails
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(1341, 690);
+            Controls.Add(dataGridView1);
+            Controls.Add(btnClose);
+            Controls.Add(btnPrint);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.SizableToolWindow;
+            Name = "StockDetails";
+            Text = "StockDetails";
+            Load += StockDetails_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button btnPrint;
+        private Button btnClose;
+        private DataGridView dataGridView1;
+    }
+}
---
+++ Blood Bank Management System/StockDetails.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Blood_Bank_Management_System
+{
+    public partial class StockDetails : Form
+    {
+        function fn = new function();
+        String query;
+        public StockDetails()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void StockDetails_Load(object sender, EventArgs e)
+        {
+            query = "select * from stock";
+            DataSet ds = fn.getData(query);
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
